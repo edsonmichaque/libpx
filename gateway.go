9ddad0a72aa5266c41971ceee76ebdad17560d43
Edson Michaque <edson@michaque.com>
2022-02-19 01:25:41 +0200

    add more Gateway functions

--- gateway.go
+++ gateway.go
@@ -5,7 +5,47 @@ type Amount struct {
 	Value    int64
 }
 
+type Option func(*Options)
+
+func WithShippingAddress(a string) Option {
+	return func(o *Options) {
+		o.ShippingAddress = a
+	}
+}
+
+func WithBillingAddress(a string) Option {
+	return func(o *Options) {
+		o.BillingAddress = a
+	}
+}
+
+func WithExtra(key, value string) Option {
+	return func(o *Options) {
+		if o.Extra == nil {
+			o.Extra = make([]Extra, 0)
+		}
+
+		o.Extra = append(o.Extra, Extra{Key: key, Value: value})
+	}
+}
+
+type Options struct {
+	ShippingAddress string
+	BillingAddress  string
+	IP              string
+	Extra           []Extra
+}
+
+type Extra struct {
+	Key   string
+	Value string
+}
+
 type Gateway interface {
-	Authorize(card Card, amount Amount, opts ...interface{}) (*Authorization, error)
-	Capture(auth Authorization, amount Amount, opts ...interface{}) (*Capture, error)
+	Authorize(card Card, amount Amount, opts ...Option) (*Authorization, error)
+	Capture(auth Authorization, amount Amount, opts ...Option) (*Capture, error)
+	Purchase(auth Authorization, amount Amount, opts ...Option) (*Capture, error)
+	Refund(auth Authorization, amount Amount) (*Capture, error)
+	Void(auth Authorization) (*Capture, error)
+	Verify(card Card, opts ...Option) (*Capture, error)
 }
