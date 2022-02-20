0bfb51f8017144c0ce56b39997b3a866b9a962d3
Edson Michaque <edson@michaque.com>
2022-02-20 15:57:29 +0200

    turns out like moving Card to root was not a good idea, I am reverting it

--- card/card.go
+++ card/card.go
@@ -1,5 +1,7 @@
 package card
 
+import "errors"
+
 type Type uint
 
 const (
@@ -14,3 +16,18 @@ const (
 	Visa Network = iota
 	MasterCard
 )
+
+type Card struct {
+	Type            Type
+	Network         Network
+	Number          string
+	ExpirationMonth string
+	ExpirationYear  string
+	CVV             string
+	FirstName       string
+	LastName        string
+}
+
+func (c Card) Validate() error {
+	return errors.New("not valid")
+}
--- provider.go
+++ provider.go
@@ -1,8 +1,6 @@
 package libpx
 
 import (
-	"errors"
-
 	"gitlab.com/edsonmichaque/libpx/card"
 	"gitlab.com/edsonmichaque/libpx/currency"
 	"gitlab.com/edsonmichaque/libpx/schema"
@@ -39,30 +37,15 @@ type Provider interface {
 	Schema() map[string]schema.Schema
 	Currencies() []currency.Currency
 	Configure(args ...ConfigOption) error
-	Authorize(Card, Amount, ...Option) (*Authorization, error)
+	Authorize(card.Card, Amount, ...Option) (*Authorization, error)
 	Capture(Authorization, Amount, ...Option) (*Capture, error)
 	Purchase(Authorization, Amount, ...Option) (*Purchase, error)
 	Refund(Authorization, Amount) (*Refund, error)
 	Void(Authorization) (*Void, error)
-	Verify(Card, ...Option) (*Verification, error)
+	Verify(card.Card, ...Option) (*Verification, error)
 }
 
 type Amount struct {
 	Currency currency.Currency
 	Value    int64
-}
-
-type Card struct {
-	Type            card.Type
-	Network         card.Network
-	Number          string
-	ExpirationMonth string
-	ExpirationYear  string
-	CVV             string
-	FirstName       string
-	LastName        string
-}
-
-func (c Card) Validate() error {
-	return errors.New("not valid")
 }
